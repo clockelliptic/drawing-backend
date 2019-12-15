c18ef3b6551866bc2a1732511e0810b9489e6fd2
torch <harall09@evergreen.edu>
2019-12-15 18:15:55 -0500

    update CORS policy

--- server.js
+++ server.js
@@ -13,18 +13,18 @@ const pusher = new Pusher({
   forceTLS: true
 });
 
+const allowCrossDomain = (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); // for POST route - incoming remote resources
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+}
+
+app.use(allowCrossDomain);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
-
-
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*'); // for POST route - incoming remote resources
-    res.header(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept'
-    );
-    next();
-  });
 
 /*
  *   - POST Route -
